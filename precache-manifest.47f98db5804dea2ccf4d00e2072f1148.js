cb89f91a2a45f9c3d773623e97395d09eb214766
Laura <lhernan53112@gmail.com>
2020-07-28 15:51:39 -0400

    Updates

---
+++ precache-manifest.47f98db5804dea2ccf4d00e2072f1148.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d6a66530f4d60d72b201cc566cabe27",
+    "url": "/Portfolio-React/index.html"
+  },
+  {
+    "revision": "59f638363d574420fb21",
+    "url": "/Portfolio-React/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "6dd1b27f791413c57728",
+    "url": "/Portfolio-React/static/js/2.280f300a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Portfolio-React/static/js/2.280f300a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59f638363d574420fb21",
+    "url": "/Portfolio-React/static/js/main.6a55d810.chunk.js"
+  },
+  {
+    "revision": "c1f405c32fd866263023",
+    "url": "/Portfolio-React/static/js/runtime-main.cc5fc537.js"
+  }
+]);
