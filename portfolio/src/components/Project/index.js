c57dcbc2f86d02f339d4634e3077e9243393b102
Laura <lhernan53112@gmail.com>
2020-07-30 17:17:24 -0400

    added Project component

---
+++ portfolio/src/components/Project/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import "./style.css";
+
+function Project (props){
+    return(
+        <div className="card">
+        <div className="img-container">
+            <a target="_blank" href={props.githubURL}>
+            <img alt={props.alt} src={props.image} />
+            </a>
+        </div>
+        <div className="content">
+          <ul>
+            <li>
+              <strong>{props.name}</strong> 
+            </li>
+            <li>
+              <strong>Technologies:</strong> {props.technologies}
+            </li>
+          </ul>
+        </div>
+      </div>
+  
+    );
+}
+
+export default Project;
