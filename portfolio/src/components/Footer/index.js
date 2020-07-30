31145a97ae9ee27c67d6b38d7d4431bc58f8f4a9
Laura Hernandez <34899738+Laura915@users.noreply.github.com>
2020-07-29 21:24:59 -0400

    Added footer (#2)

---
+++ portfolio/src/components/Footer/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Footer(){
+    return(
+        <footer className="row fixed-bottom">
+        <div className=" col-lg-12 footer ">
+            <p className="text-center">
+            2020 @ Copyright Laura Hernandez. All Rights Reserved
+            </p>
+        </div>
+    </footer>
+    );
+}
+
+export default Footer;
