20ff57013d98c29836421362d48a290030b79046
Laura <lhernan53112@gmail.com>
2020-08-01 13:15:12 -0400

    Imported Works component

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -2,6 +2,8 @@
 import React, { Component } from 'react';
 import { HashRouter, Route} from 'react-router-dom';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Works from './components/Works';
 
 class App extends Component {
   render() {
@@ -15,6 +17,7 @@ class App extends Component {
           <Route exact path="/Works" component={Works} />
           <Route exact path="/Contact" component={Contact} />
           
+          <Footer/>
         </div>
       </HashRouter>
     );
@@ -23,7 +26,7 @@ class App extends Component {
 
 const Home = () => <div><h2>Home</h2></div>
 const About = () => <div><h2>About Me</h2></div>
-const Works = () => <div><h2>Works</h2></div>
+// const Works = () => <div><h2>Works</h2></div>
 const Contact = () => <div><h2>Contact</h2></div>
 
 export default App;
