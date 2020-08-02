0990e6ec8869b54f138db79f5a216984d4f2618d
Laura <lhernan53112@gmail.com>
2020-08-02 18:13:40 -0400

    Edited Works Array

--- portfolio/src/components/Works/index.js
+++ portfolio/src/components/Works/index.js
@@ -1,22 +1,60 @@
 import React from 'react';
-import Project from 'react';
-// import "../../assets";
+import Project from '../Project';
+
+import savor from "../../assets/savor.png";
+import scheduler from "../../assets/scheduler.png";
+import passwordGen from "../../assets/passwordGen.png";
+import noteTaker from "../../assets/note-taker.png";
+import burger from "../../assets/burger.png";
+import trainingZone from "../../assets/training-zone.png";
 
 let WorksArr = [
   {
     id: 1,
     name: "Savor",
-    image: "savor.png",
+    image: savor,
     altMessage: "Savor app with display of Ciara's Level up lyrics",
     technologies: "Materialize HTML JavaScript JQuery CSS ItunesAPI lyrics.ovhAPI",
     github_URL: "https://caseyvanalstyne.github.io/Savor"
   },
   {
     id: 2,
-    name: "Work Day scheduler App",
-    image: "scheduler.png",
+    name: "Work Day scheduler",
+    image: scheduler,
     altMessage: "calendar app with list of inputs from 9-5",
     technologies: "HTML JavaScript JQuery CSS ",
+    github_URL: "https://laura915.github.io/Work-Day-Scheduler/"
+  },
+  {
+    id: 3,
+    name: "Password Generator",
+    image: passwordGen,
+    altMessage: "Button and output area with generated password",
+    technologies: "HTML JavaScript CSS ",
+    github_URL: "https://laura915.github.io/Password-Generator/"
+  },
+  {
+    id: 4,
+    name: "Burger",
+    image: burger,
+    altMessage: "Burger app with available and finished burgers list",
+    technologies: "HTML JavaScript JQuery CSS express-handlebars mysql Node",
+    github_URL: "https://secure-beyond-70824.herokuapp.com/"
+  },
+  {
+    id: 5,
+    name: "Training Zone",
+    image: trainingZone,
+    altMessage: "2 weekly work out cards",
+    technologies: "HTML JavaScript JQuery CSS express-handlebars mysql",
+    github_URL: "https://laura915.github.io/Work-Day-Scheduler/"
+  },
+  {
+    id: 6,
+    name: "Note-Taker",
+    image: noteTaker,
+    altMessage: "Note text area and lists of todos",
+    technologies: "HTML JavaScript JQuery CSS",
     github_URL: "https://laura915.github.io/Work-Day-Scheduler/"
   }
 ]
@@ -24,7 +62,7 @@ let WorksArr = [
 function Works() {
 
   return (
-    <>
+    <div>
       {WorksArr.map(work => (
         <Project
           key={work.id}
@@ -35,7 +73,7 @@ function Works() {
           github_URL={work.github_URL}
         />
       ))}
-    </>
+    </div>
   );
 }
 
