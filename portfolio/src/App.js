349727ae1182de130669056b313e8d1474422f00
Laura <lhernan53112@gmail.com>
2020-08-27 12:33:53 -0400

    Home page footer sticks to bottom

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,26 +1,34 @@
 
 import React, { Component } from 'react';
-import { HashRouter, Route} from 'react-router-dom';
+import { HashRouter, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Works from './components/Works';
 import AboutMe from './components/AboutMe';
 import Contact from './components/Contact';
 import Home from './components/Home';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
       <HashRouter basename="/">
         <div>
-          <Navbar/>
-  
-          <Route exact path="/" component={Home} />
-          <Route exact path="/About Me" component={AboutMe} />
-          <Route exact path="/Works" component={Works} />
-          <Route exact path="/Contact" component={Contact} />
-          
-          <Footer/>
+          <Navbar />
+
+          <div id="page-container">
+
+            <div id="content-wrap">
+              <Route exact path="/" component={Home} />
+              <Route exact path="/About Me" component={AboutMe} />
+              <Route exact path="/Works" component={Works} />
+              <Route exact path="/Contact" component={Contact} />
+            </div>
+
+            <Footer  />
+          </div>
+
+
         </div>
       </HashRouter>
     );
--- portfolio/src/components/Footer/index.js
+++ portfolio/src/components/Footer/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import "./style.css";
 
 function Footer(){
     return(
-        <footer className="row fixed-bottom">
-        <div className=" col-lg-12 footer ">
+        <footer className="row " id="footer">
+        <div className=" col-lg-12 ">
             <p className="text-center">
             2020 @ Copyright Laura Hernandez. All Rights Reserved
             </p>
         </div>
-    </footer>
+         </footer>
     );
 }
 
