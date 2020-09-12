44fe9b3f2d406006bf0891036eaf37890560ff6c
Laura Hernandez <34899738+Laura915@users.noreply.github.com>
2020-09-11 20:00:53 -0400

    Edit links (#12)
    * Works links link to the coreect page
    
    * Added newest project
    
    * Added resume link
    
    * Edited burger screenshot
    
    * Added a github link to the project component
    
    * Edited link to website now it's links below card  and not from the picture
    
    * Updated readme

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Portfolio-React
+ ### [Portfolio link !](https://laura915.github.io/Portfolio-React/)
+
+  ## Build 
+  **Technologies Used :** </br> JavaScript JQuery Node HTML CSS Bootstrap </br>
+  **Process:** 
+  1. Installed New React App using Node JS.
+  2. Deployed app using GitHub Pages
+  3. Applied [react-router-dom](https://www.npmjs.com/package/react-router-dom) package to route from one page to another
+  4. Utilized [React](https://reactjs.org/) props and created a project component that applies the html. The project component is imported and used in the works component where all the data is mapped onto a card accordingly
+  5. The rest of the componets are static content. Each component is exported with all it's content to the App.js 
+  6. Added a background pic from [Unsplash](https://unsplash.com/t/textures-patterns) and applied it to the home page 
+  7. Created a unique favicon on [favicon.io](https://favicon.io/) that matched portfolio theme
+  8. Used [boostrap](https://getbootstrap.com/docs/4.5/components/alerts/) for starter html componets code and added extra css styling based on screen size responsivenes 
+
+  ## Questions
+  If you have a question about this repo, open a issue or contact Laura915 
--- portfolio/src/components/Project/index.js
+++ portfolio/src/components/Project/index.js
@@ -3,23 +3,27 @@ import "./style.css";
 
 function Project(props) {
   return (
-      <div className="card" >
-        <div className="img-container">
-          <a target="_blank" href={props.github_URL}>
-            <img className="card-img-top" alt={props.altMessage} src={props.image} />
-          </a>
-        </div>
-        <div className="content" >
-          <ul>
-            <li>
-              <strong>{props.name}</strong>
-            </li>
-            <li>
-              <strong>Technologies:</strong> {props.technologies}
-            </li>
-          </ul>
-        </div>
+    <div className="card" >
+      <div className="img-container">
+          <img className="card-img-top" alt={props.altMessage} src={props.image} />
       </div>
+      <div className="content" >
+        <ul>
+          <li>
+            <strong>{props.name}</strong>
+          </li>
+          <li>
+            <strong>Technologies:</strong> {props.technologies}
+          </li>
+          <li className="card-links">
+            <a target="_blank" href={props.app_URL} class="card-link">Website</a>
+          </li>
+          <li className="card-links github-link">
+            <a target="_blank" href={props.github_URL} class="card-link">GitHub</a>
+          </li>
+        </ul>
+      </div>
+    </div>
   );
 }
 
